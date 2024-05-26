14b869a06c6653ae35a6d161b6208afab363d0ae
Viachaslau <maltsev.v.v@hotmail.com>
2024-05-26 17:57:51 +0300

    Add files via upload

--- autoinstall.sh
+++ autoinstall.sh
@@ -111,6 +111,13 @@ fi
 # fi
 
 #VERSION="12 (bookworm)
+if grep -q 'VERSION="12 (bookworm)"' /etc/os-release; then
+  echo "---------------------------------------------------------"
+  echo "Installing OverlayFS"
+  echo "---------------------------------------------------------"
+  apt-get install -y overlayroot
+fi
+
 
 echo "---------------------------------------------------------"
 echo "Installing Python-can"
@@ -305,11 +312,12 @@ if grep -q 'VERSION="10 (buster)"' /etc/os-release; then
   sed -i -e '$i \  <addon optional="true">repository.maltsev_kodi-18</addon>' /usr/share/kodi/system/addon-manifest.xml
 elif grep -q 'VERSION="11 (bullseye)"' /etc/os-release; then
   wget -P /tmp https://github.com/maltsevvv/repository-kodi/raw/master/kodi19/skin.audi_rns/skin.audi_rns-19.3.0.zip
-  wget -P /tmp https://github.com/maltsevvv/repository-kodi/raw/master/repository.maltsev_kodi-19/repository.maltsev_kodi-19-1.0.0.zip
+  wget -P /tmp https://github.com/maltsevvv/repository-kodi/raw/master/repository.maltsev_kodi-19/repository.maltsev_kodi-1.0.0.zip
   sed -i -e '$i \  <addon>repository.maltsev_kodi-19</addon>' /usr/share/kodi/system/addon-manifest.xml
 elif grep -q 'VERSION="12 (bookworm)"' /etc/os-release; then
-  wget -P /tmp https://github.com/maltsevvv/repository-kodi/raw/master/kodi20/skin.audi_rns/skin.audi_rns-20.3.0.zip
-  wget -P /tmp https://github.com/maltsevvv/repository-kodi/raw/master/repository.maltsev_kodi-20/repository.maltsev_kodi-20-1.0.0.zip
+  #wget -P /tmp https://github.com/maltsevvv/repository-kodi/raw/master/kodi20/skin.audi_rns/skin.audi_rns-20.3.0.zip
+  wget -P /tmp https://github.com/maltsevvv/repository-kodi/raw/master/kodi20/skin.audi_rns/skin.audi_rns-20.3.1.zip
+  wget -P /tmp https://github.com/maltsevvv/repository-kodi/raw/master/repository.maltsev_kodi-20/repository.maltsev_kodi-1.0.0.zip
   sed -i -e '$i \  <addon>repository.maltsev_kodi-20</addon>' /usr/share/kodi/system/addon-manifest.xml
 fi
 rm -r /home/pi/.kodi/addons/skin.audi_rns*
